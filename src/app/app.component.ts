37f055d5ce8720b6ef756ab6f0d4ca60b37941df
Harshdeep Singh Plaha <75137919+Harshdeepplaha@users.noreply.github.com>
2022-06-15 14:09:03 +0530

    how to get access to the form object and use it with data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,15 @@ import { Component, ElementRef } from '@angular/core';
 })
 export class AppComponent {
   title = 'learning-forms';
+  defaultQuestion = 'your first pet?';
+  answer = '';
 
-onSubmit(form: ElementRef){
-  console.log(form)
+
+onSubmit(form: NgForm){
+  console.log(form);
+
+}
 }
 
 
-}
+
